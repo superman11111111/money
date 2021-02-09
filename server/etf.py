614b81248c25d3b1acde688f4183fd03f80e1a41
mkypr <superman222222223333333@gmail.com>
2021-02-09 19:08:20 +0100

    remove

--- server/etf.py
+++ server/etf.py
@@ -96,7 +96,6 @@ class MyETF:
             for col in self.cols:
                 replace_columns[self.cols[col]] = col
             tmp = tmp.rename(columns=replace_columns)
-            print(tmp)
             for j in range(len(tmp['ticker'])):
                 tmptk = tmp['ticker'].iloc[j]
                 if pd.isnull(tmptk):
